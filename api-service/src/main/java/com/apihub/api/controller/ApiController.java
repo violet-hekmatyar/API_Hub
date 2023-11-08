b13f3de309140cbf4973e5d35d377dec83812a0c
hekmatyar_violet <1355609295@qq.com>
2023-11-08 17:11:33 +0800

    feat: post请求转发

--- api-service/src/main/java/com/apihub/api/controller/ApiController.java
+++ api-service/src/main/java/com/apihub/api/controller/ApiController.java
@@ -15,13 +15,9 @@ import com.apihub.common.utils.UserHolder;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/apiService")
@@ -30,7 +26,7 @@ import java.util.Objects;
 public class ApiController {
     private final InterfaceInfoServiceClient interfaceInfoServiceClient;
 
-    @ApiOperation("获取接口信息+校验")
+    @ApiOperation("get请求接口")
     @GetMapping("/get")
     public BaseResponse<Object> getInterfaceInfoById(@RequestParam("InterfaceId") long interfaceId,
                                                      @RequestParam("body") String body,
@@ -46,7 +42,7 @@ public class ApiController {
             //System.out.println("id===0---------------------------------------------");
             throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "服务器繁忙，请稍后重试");
         }
-        if (!Objects.equals(interfaceInfo.getMethod(), "get")) {
+        if (!interfaceInfo.getMethod().contains("get")) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求方法错误");
         }
         //todo 查询是否符合请求规范
@@ -61,6 +57,43 @@ public class ApiController {
         //向url发请求
         String requestBody = URLUtil.decode(body, CharsetUtil.CHARSET_UTF_8);
         HttpResponse httpResponse = HttpRequest.get(interfaceInfo.getUrl() + "?" + requestBody)
+                .execute();
+
+        //todo 扣减用户额度
+        //请求成功，扣钱，使用MQ队列
+
+        return ResultUtils.success(httpResponse.body());
+    }
+
+    @ApiOperation("post请求接口")
+    @PostMapping("/post")
+    public BaseResponse<Object> postInterfaceInfoById(@RequestParam("InterfaceId") long interfaceId,
+                                                     @RequestParam("body") String body,
+                                                     HttpServletRequest request) {
+        Long userId = UserHolder.getUser();
+        //查询接口url及其他信息
+        InterfaceInfo interfaceInfo = interfaceInfoServiceClient.queryItemById(interfaceId);
+
+        if (interfaceInfo == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "未查询到有效接口");
+        }
+        if (interfaceInfo.getId() == 0) {
+            //System.out.println("id===0---------------------------------------------");
+            throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "服务器繁忙，请稍后重试");
+        }
+        if (!interfaceInfo.getMethod().contains("post")) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求方法错误");
+        }
+
+        //todo 查询用户可用次数---带价格
+        //如果不够次数，直接返回结果
+
+        //todo 根据interface的类别进行分别操作
+
+
+        //向url发请求
+        String requestBody = URLUtil.decode(body, CharsetUtil.CHARSET_UTF_8);
+        HttpResponse httpResponse = HttpRequest.post(interfaceInfo.getUrl() + "?" + requestBody)
                 .execute();
 
         //todo 扣减用户额度
