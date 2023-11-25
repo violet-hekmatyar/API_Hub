7a2f170b5bad92476a1b20da83adbc025f6ce553
hekmatyar_violet <1355609295@qq.com>
2023-11-25 10:43:54 +0800

    fix: api第三方接口调用功能完善2
    测试通过
    查询+扣款服务抽离成函数
    MQ队列发送抽离成函数
    添加查询客户端IP功能

--- api-service/src/main/java/com/apihub/api/controller/ApiController.java
+++ api-service/src/main/java/com/apihub/api/controller/ApiController.java
@@ -9,6 +9,7 @@ import com.apihub.api.model.domain.InterfaceInfo;
 import com.apihub.api.model.dto.DeductOrderMqDTO;
 import com.apihub.api.openFeign.client.InterfaceInfoServiceClient;
 import com.apihub.api.openFeign.client.UserServiceClient;
+import com.apihub.api.utils.GetIpAddressUtils;
 import com.apihub.common.common.BaseResponse;
 import com.apihub.common.common.ErrorCode;
 import com.apihub.common.common.ResultUtils;
@@ -41,24 +42,7 @@ public class ApiController {
         //查询接口url及其他信息
         InterfaceInfo interfaceInfo = interfaceInfoServiceClient.queryItemById(interfaceId);
 
-        if (interfaceInfo == null) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "未查询到有效接口");
-        }
-        if (interfaceInfo.getId() == 0) {
-            //System.out.println("id===0---------------------------------------------");
-            throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "服务器繁忙，请稍后重试");
-        }
-        if (!interfaceInfo.getMethod().contains("get")) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求方法错误");
-        }
-        Integer interfaceInfoPrice = interfaceInfo.getPrice();
-        //对于第三方接口，不检查是否符合请求规范
-
-        //检查用户余额，并进行扣款
-        if (!userServiceClient.deductBalance(interfaceInfoPrice)) {
-            //如果不够次数，直接返回结果
-            throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "用户余额不足");
-        }
+        getInfoAndDeduct(interfaceInfo, "get");
 
         //向url发请求
         String requestBody = URLUtil.decode(body, CharsetUtil.CHARSET_UTF_8);
@@ -66,20 +50,7 @@ public class ApiController {
                 .execute();
 
         //请求成功，使用MQ队列
-        try {
-            DeductOrderMqDTO deductOrderMqDTO = new DeductOrderMqDTO();
-            deductOrderMqDTO.setInterfaceId(interfaceInfo.getId());
-            deductOrderMqDTO.setNum(1L);
-            deductOrderMqDTO.setPaymentType(3);
-            deductOrderMqDTO.setTotalFee(interfaceInfoPrice);
-            //返回发送请求的客户端或最后一个代理的Internet协议（IP）地址
-            deductOrderMqDTO.setUserAddress(request.getRemoteAddr());
-            deductOrderMqDTO.setUserId(UserHolder.getUser());
-            log.info("发送MQ队列请求");
-            rabbitTemplate.convertAndSend("payService.topic", "order.success", deductOrderMqDTO);
-        } catch (Exception e) {
-            log.error("MQ队列出错，订单发送失败：", e);
-        }
+        sendMQ(interfaceInfo, request);
 
         return ResultUtils.success(httpResponse.body());
     }
@@ -89,10 +60,23 @@ public class ApiController {
     public BaseResponse<Object> postInterfaceInfoById(@RequestParam("InterfaceId") long interfaceId,
                                                       @RequestParam("body") String body,
                                                       HttpServletRequest request) {
-        Long userId = UserHolder.getUser();
         //查询接口url及其他信息
         InterfaceInfo interfaceInfo = interfaceInfoServiceClient.queryItemById(interfaceId);
 
+        getInfoAndDeduct(interfaceInfo, "post");
+
+        //向url发请求
+        String requestBody = URLUtil.decode(body, CharsetUtil.CHARSET_UTF_8);
+        HttpResponse httpResponse = HttpRequest.post(interfaceInfo.getUrl() + "?" + requestBody)
+                .execute();
+
+        //请求成功，使用MQ队列
+        sendMQ(interfaceInfo, request);
+
+        return ResultUtils.success(httpResponse.body());
+    }
+
+    private void getInfoAndDeduct(InterfaceInfo interfaceInfo, String method) {
         if (interfaceInfo == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "未查询到有效接口");
         }
@@ -100,7 +84,7 @@ public class ApiController {
             //System.out.println("id===0---------------------------------------------");
             throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "服务器繁忙，请稍后重试");
         }
-        if (!interfaceInfo.getMethod().contains("post")) {
+        if (!interfaceInfo.getMethod().contains(method)) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求方法错误");
         }
         Integer interfaceInfoPrice = interfaceInfo.getPrice();
@@ -109,12 +93,9 @@ public class ApiController {
             //如果不够次数，直接返回结果
             throw new BusinessException(ErrorCode.FORBIDDEN_ERROR, "用户余额不足");
         }
+    }
 
-        //向url发请求
-        String requestBody = URLUtil.decode(body, CharsetUtil.CHARSET_UTF_8);
-        HttpResponse httpResponse = HttpRequest.post(interfaceInfo.getUrl() + "?" + requestBody)
-                .execute();
-
+    private void sendMQ(InterfaceInfo interfaceInfo, HttpServletRequest request) {
         //请求成功，使用MQ队列
         //仅记录订单
         //使用try catch，因为错误记录可以人工更改，但是钱必须先扣
@@ -123,16 +104,14 @@ public class ApiController {
             deductOrderMqDTO.setInterfaceId(interfaceInfo.getId());
             deductOrderMqDTO.setNum(1L);
             deductOrderMqDTO.setPaymentType(3);
-            deductOrderMqDTO.setTotalFee(interfaceInfoPrice);
+            deductOrderMqDTO.setTotalFee(interfaceInfo.getPrice());
             //返回发送请求的客户端或最后一个代理的Internet协议（IP）地址
-            deductOrderMqDTO.setUserAddress(request.getRemoteAddr());
+            deductOrderMqDTO.setUserAddress(GetIpAddressUtils.GetIpAddress(request));
             deductOrderMqDTO.setUserId(UserHolder.getUser());
             log.info("发送MQ队列请求");
             rabbitTemplate.convertAndSend("payService.topic", "order.success", deductOrderMqDTO);
         } catch (Exception e) {
             log.error("MQ队列出错，订单发送失败：", e);
         }
-
-        return ResultUtils.success(httpResponse.body());
     }
 }
--- api-service/src/main/java/com/apihub/api/openFeign/client/UserServiceClient.java
+++ api-service/src/main/java/com/apihub/api/openFeign/client/UserServiceClient.java
@@ -2,7 +2,7 @@ package com.apihub.api.openFeign.client;
 
 import com.apihub.api.openFeign.config.DefaultFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserServiceClient {
 
     //获取接口信息
-    @GetMapping("/api/user/balance/deduct")
+    @PutMapping("/api/user/balance/deduct")
     Boolean deductBalance(@RequestParam("amount") Integer amount);
 }
