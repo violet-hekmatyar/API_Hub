b9d9f4a8ab73d7494dfc40f76d28625d1f0629e4
hekmatyar_violet <1355609295@qq.com>
2023-12-25 20:59:47 +0800

    fix: 网关跨域修复2
    springSecurity 跨域解决方案

--- gateway/src/main/java/com/apihub/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/apihub/gateway/GatewayApplication.java
@@ -3,8 +3,10 @@ package com.apihub.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
---
+++ gateway/src/main/java/com/apihub/gateway/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.apihub.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .mvcMatchers("/api").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                // 跨域配置
+                .and()
+                .cors()
+                .configurationSource(configurationSource());
+    }
+
+    CorsConfigurationSource configurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
+
