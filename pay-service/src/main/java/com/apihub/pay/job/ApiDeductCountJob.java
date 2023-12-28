f35f3c88c8ce887542ac6b57cada2376907af58a
hekmatyar_violet <1355609295@qq.com>
2023-12-28 14:06:48 +0800

    chore: 引入redis,移除ApiOrder部分
    使ApiOrder部分失效，后续将删除ApiOrder部分
    使用redis来记录用户的api使用情况（今日）
    将设想以注释写了一下

---
+++ pay-service/src/main/java/com/apihub/pay/job/ApiDeductCountJob.java
@@ -0,0 +1,14 @@
+package com.apihub.pay.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+//将每天的redis数据同步到MySQL中
+@Component
+@Slf4j
+public class ApiDeductCountJob {
+
+    //写入mysql
+
+    //删除redis数据
+}
