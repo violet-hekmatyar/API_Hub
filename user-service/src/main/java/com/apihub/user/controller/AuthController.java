56b4fd0db985f19a20740378b5fed41eb21d9331
hekmatyar_violet <1355609295@qq.com>
2024-04-03 10:29:58 +0800

    feat: gitee第三方登录
    成功跑通第三方登录，需要进一步使用返回的数据

---
+++ user-service/src/main/java/com/apihub/user/controller/AuthController.java
@@ -0,0 +1,61 @@
+package com.apihub.user.controller;
+
+
+import cn.hutool.json.JSONUtil;
+import com.xkcoding.justauth.AuthRequestFactory;
+import lombok.extern.slf4j.Slf4j;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/user/oauth")
+public class AuthController {
+    @Autowired
+    private AuthRequestFactory factory;
+
+    @GetMapping
+    public List<String> list() {
+        return factory.oauthList();
+    }
+
+    @GetMapping("/login/gitee")
+    public void giteeLogin(HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = factory.get("gitee");
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @GetMapping("/{type}/callback")
+    public AuthResponse login(@PathVariable String type, AuthCallback callback) {
+        AuthRequest authRequest = factory.get(type);
+        AuthResponse response = authRequest.login(callback);
+        log.info("【response】= {}", JSONUtil.toJsonStr(response));
+        return response;
+    }
+
+    /*@GetMapping("/login/{type}")
+    public void login(@PathVariable String type, HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = factory.get(type);
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @GetMapping("/{type}/callback")
+    public AuthResponse login(@PathVariable String type, AuthCallback callback) {
+        AuthRequest authRequest = factory.get(type);
+        AuthResponse response = authRequest.login(callback);
+        log.info("【response】= {}", JSONUtil.toJsonStr(response));
+        return response;
+    }*/
+
+}
